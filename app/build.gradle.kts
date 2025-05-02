54fc44d34b63e50d1b2b8d41a4748234d005d5cc
Ana Cristina <anacrispee@edu.univali.br>
2025-05-02 15:33:45 -0300

    refactor: apply gradle plugin in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,38 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("custom-gradle-plugin")
 }
 
 android {
     namespace = "com.example.pluginsample"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.pluginsample"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
--- buildSrc/src/main/kotlin/CustomGradlePlugin.kt
+++ buildSrc/src/main/kotlin/CustomGradlePlugin.kt
@@ -1,3 +1,5 @@
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
@@ -12,32 +14,26 @@ class CustomGradlePlugin : Plugin<Project> {
 
     private fun applyPlugins(project: Project) {
         project.apply {
-            plugin("android-library")
             plugin("kotlin-android")
         }
     }
 
     private fun setProjectConfig(project: Project) {
         project.android().apply {
+
+            compileSdkVersion(35)
+
             defaultConfig {
-                minSdk = ProjectConfig.MIN_SDK
-                compileSdk = ProjectConfig.COMPILE_SDK
+                minSdk = 24
+                targetSdk = 35
+                versionCode = 1
+                versionName = "1.0"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             }
 
             compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_1_8
-                targetCompatibility = JavaVersion.VERSION_1_8
-            }
-
-            buildTypes {
-                release {
-                    isMinifyEnabled = false
-                    proguardFiles(
-                        getDefaultProguardFile("proguard-android-optimize.txt"),
-                        "proguard-rules.pro"
-                    )
-                }
+                sourceCompatibility = JavaVersion.VERSION_18
+                targetCompatibility = JavaVersion.VERSION_18
             }
 
             project.tasks.withType(KotlinCompile::class.java).configureEach {
@@ -45,10 +41,33 @@ class CustomGradlePlugin : Plugin<Project> {
                     jvmTarget = "18"
                 }
             }
+
+            val proguardFile = "proguard-rules.pro"
+            when (this) {
+                is LibraryExtension -> defaultConfig {
+                    consumerProguardFiles(proguardFile)
+                }
+
+                is AppExtension -> buildTypes {
+                    getByName("release") {
+                        isMinifyEnabled = true
+                        isShrinkResources = true
+                        debuggable(false)
+                        proguardFile(proguardFile)
+                    }
+
+                    getByName("debug") {
+                        isMinifyEnabled = false
+                        isShrinkResources = false
+                        debuggable(true)
+                        proguardFile(proguardFile)
+                    }
+                }
+            }
         }
     }
 
-    private fun Project.android() : LibraryExtension {
-        return extensions.getByType(LibraryExtension::class.java)
+    private fun Project.android() : BaseExtension {
+        return project.extensions.getByName("android") as BaseExtension
     }
 }
